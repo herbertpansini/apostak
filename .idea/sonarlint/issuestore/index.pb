c0bb757afd3a1773ca211214c2dccab6a05a06ec
Herbert <herbert.andrade@pdcase.com.br>
2021-05-23 10:01:41 -0300

    Initial commit.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+i
+9app/src/main/java/com/lacerda/thiago/entities/Escudo.java,e/f/ef5192af21e2a2facc28a6c61fe4ce752540ae77
---
+++ app/src/main/java/com/lacerda/thiago/apostak/ConfiguracaoActivity.java
@@ -0,0 +1,13 @@
+package com.lacerda.thiago.apostak;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfiguracaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracao);
+    }
+}
